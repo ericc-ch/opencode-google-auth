1012d9795a4efd99332e853d0ed920bb385aca25
Erick Christian <erickchristian48@gmail.com>
2025-12-26 21:10:03 +0700

    chore: remove reference code

--- src/cli.ts
+++
@@ -1,8 +0,0 @@
-import yargs from "yargs"
-import { hideBin } from "yargs/helpers"
-
-await yargs(hideBin(process.argv))
-  .scriptName("pkg-placeholder")
-  .usage("$0 <cmd> [args]")
-  .help()
-  .parse()
